4050afca854f6d9803cfe7374d45119b738c19ae
mahmudx7 <mahmudx077@gmail.com>
2025-03-02 23:25:01 +0600

    Update count.js

--- scripts/cmds/count.js
+++ scripts/cmds/count.js
@@ -1,166 +1,129 @@
 module.exports = {
-	config: {
-		name: "count",
-		aliases: ["c"],
-		version: "1.3",
-		author: "NTKhang",
-		countDown: 5,
-		role: 0,
-		description: {
-			vi: "Xem số lượng tin nhắn của tất cả thành viên hoặc bản thân (tính từ lúc bot vào nhóm)",
-			en: "View the number of messages of all members or yourself (since the bot joined the group)"
-		},
-		category: "box chat",
-		guide: {
-			vi: "   {pn}: dùng để xem số lượng tin nhắn của bạn"
-				+ "\n   {pn} @tag: dùng để xem số lượng tin nhắn của những người được tag"
-				+ "\n   {pn} all: dùng để xem số lượng tin nhắn của tất cả thành viên",
-			en: "   {pn}: used to view the number of messages of you"
-				+ "\n   {pn} @tag: used to view the number of messages of those tagged"
-				+ "\n   {pn} all: used to view the number of messages of all members"
-		}
-	},
+    config: {
+        name: "count",
+        aliases: ["c"],
+        version: "1.4", 
+        author: "NTKhang",
+        countDown: 5,
+        role: 0,
+        description: "View the number of messages of all members or yourself (since the bot joined the group)",
+        category: "box chat",
+        guide: "{pn}: used to view the number of messages of you\n"
+            + "{pn} @tag: used to view the number of messages of those tagged\n"
+            + "{pn} all: used to view the number of messages of all members"
+    },
 
-	langs: {
-		vi: {
-			count: "Số tin nhắn của các thành viên:",
-			endMessage: "Những người không có tên trong danh sách là chưa gửi tin nhắn nào.",
-			page: "Trang [%1/%2]",
-			reply: "Phản hồi tin nhắn này kèm số trang để xem tiếp",
-			result: "%1 hạng %2 với %3 tin nhắn",
-			yourResult: "Bạn đứng hạng %1 và đã gửi %2 tin nhắn trong nhóm này",
-			invalidPage: "Số trang không hợp lệ"
-		},
-		en: {
-			count: "Number of messages of members:",
-			endMessage: "Those who do not have a name in the list have not sent any messages.",
-			page: "Page [%1/%2]",
-			reply: "Reply to this message with the page number to view more",
-			result: "%1 rank %2 with %3 messages",
-			yourResult: "You are ranked %1 and have sent %2 messages in this group",
-			invalidPage: "Invalid page number"
-		}
-	},
+    langs: {
+        en: {
+            count: "Number of messages of members:",
+            endMessage: "Those who do not have a name in the list have not sent any messages.",
+            page: "Page [%1/%2]",
+            reply: "Reply to this message with the page number to view more",
+            result: "%1 rank %2 with %3 messages",
+            yourResult: "You are ranked %1 and have sent %2 messages in this group",
+            invalidPage: "Invalid page number"
+        }
+    },
 
-	onStart: async function ({ args, threadsData, message, event, api, commandName, getLang }) {
-		const { threadID, senderID } = event;
-		const threadData = await threadsData.get(threadID);
-		const { members } = threadData;
-		const usersInGroup = (await api.getThreadInfo(threadID)).participantIDs;
-		let arraySort = [];
-		for (const user of members) {
-			if (!usersInGroup.includes(user.userID))
-				continue;
-			const charac = "️️️️️️️️️️️️️️️️️"; // This character is banned from facebook chat (it is not an empty string)
-			arraySort.push({
-				name: user.name.includes(charac) ? `Uid: ${user.userID}` : user.name,
-				count: user.count,
-				uid: user.userID
-			});
-		}
-		let stt = 1;
-		arraySort.sort((a, b) => b.count - a.count);
-		arraySort.map(item => item.stt = stt++);
+    onStart: async function ({ args, threadsData, message, event, api, commandName, getLang }) {
+        const { threadID, senderID } = event;
+        const threadData = await threadsData.get(threadID);
+        const { members } = threadData;
+        const usersInGroup = (await api.getThreadInfo(threadID)).participantIDs;
+        let arraySort = [];
 
-		if (args[0]) {
-			if (args[0].toLowerCase() == "all") {
-				let msg = getLang("count");
-				const endMessage = getLang("endMessage");
-				for (const item of arraySort) {
-					if (item.count > 0)
-						msg += `\n${item.stt}/ ${item.name}: ${item.count}`;
-				}
+        // Sort members and filter out those who aren't in the group
+        for (const user of members) {
+            if (!usersInGroup.includes(user.userID)) continue;
+            arraySort.push({
+                name: user.name,
+                count: user.count,
+                uid: user.userID
+            });
+        }
 
-				if ((msg + endMessage).length > 19999) {
-					msg = "";
-					let page = parseInt(args[1]);
-					if (isNaN(page))
-						page = 1;
-					const splitPage = global.utils.splitPage(arraySort, 50);
-					arraySort = splitPage.allPage[page - 1];
-					for (const item of arraySort) {
-						if (item.count > 0)
-							msg += `\n${item.stt}/ ${item.name}: ${item.count}`;
-					}
-					msg += getLang("page", page, splitPage.totalPage)
-						+ `\n${getLang("reply")}`
-						+ `\n\n${endMessage}`;
+        let stt = 1;
+        arraySort.sort((a, b) => b.count - a.count);
+        arraySort.forEach(item => item.stt = stt++);
 
-					return message.reply(msg, (err, info) => {
-						if (err)
-							return message.err(err);
-						global.GoatBot.onReply.set(info.messageID, {
-							commandName,
-							messageID: info.messageID,
-							splitPage,
-							author: senderID
-						});
-					});
-				}
-				message.reply(msg);
-			}
-			else if (event.mentions) {
-				let msg = "";
-				for (const id in event.mentions) {
-					const findUser = arraySort.find(item => item.uid == id);
-					msg += `\n${getLang("result", findUser.name, findUser.stt, findUser.count)}`;
-				}
-				message.reply(msg);
-			}
-		}
-		else {
-			const findUser = arraySort.find(item => item.uid == senderID);
-			return message.reply(getLang("yourResult", findUser.stt, findUser.count));
-		}
-	},
+        if (args[0]) {
+            if (args[0].toLowerCase() === "all") {
+                // Full count list in desired format
+                let msg = getLang("count");
+                const endMessage = getLang("endMessage");
 
-	onReply: ({ message, event, Reply, commandName, getLang }) => {
-		const { senderID, body } = event;
-		const { author, splitPage } = Reply;
-		if (author != senderID)
-			return;
-		const page = parseInt(body);
-		if (isNaN(page) || page < 1 || page > splitPage.totalPage)
-			return message.reply(getLang("invalidPage"));
-		let msg = getLang("count");
-		const endMessage = getLang("endMessage");
-		const arraySort = splitPage.allPage[page - 1];
-		for (const item of arraySort) {
-			if (item.count > 0)
-				msg += `\n${item.stt}/ ${item.name}: ${item.count}`;
-		}
-		msg += getLang("page", page, splitPage.totalPage)
-			+ "\n" + getLang("reply")
-			+ "\n\n" + endMessage;
-		message.reply(msg, (err, info) => {
-			if (err)
-				return message.err(err);
-			message.unsend(Reply.messageID);
-			global.GoatBot.onReply.set(info.messageID, {
-				commandName,
-				messageID: info.messageID,
-				splitPage,
-				author: senderID
-			});
-		});
-	},
+                // For members who have sent messages
+                for (const item of arraySort) {
+                    if (item.count > 0) {
+                        let rankEmoji = item.stt === 1 ? "🥇" : item.stt === 2 ? "🥈" : item.stt === 3 ? "🥉" : item.stt + ".";
+                        msg += `\n${rankEmoji} ${toBoldUnicode(item.name)}: ${toBoldNumbers(item.count)}`;
+                    }
+                }
 
-	onChat: async ({ usersData, threadsData, event }) => {
-		const { senderID, threadID } = event;
-		const members = await threadsData.get(threadID, "members");
-		const findMember = members.find(user => user.userID == senderID);
-		if (!findMember) {
-			members.push({
-				userID: senderID,
-				name: await usersData.getName(senderID),
-				nickname: null,
-				inGroup: true,
-				count: 1
-			});
-		}
-		else
-			findMember.count += 1;
-		await threadsData.set(threadID, members, "members");
-	}
+                return message.reply(msg + `\n\n${endMessage}`);
+            } else if (event.mentions) {
+                // Mentioned user(s) will have bold format
+                let msg = "";
+                for (const id in event.mentions) {
+                    const findUser = arraySort.find(item => item.uid == id);
+                    if (findUser) {
+                        msg += `\n${toBoldUnicode(findUser.name)}: 🎀 ${toBoldNumbers(findUser.count)} messages`;
+                    }
+                }
+                return message.reply(msg);
+            }
+        } else {
+            // Self-count message in bold format with specific rank display
+            const findUser = arraySort.find(item => item.uid == senderID);
+            if (findUser) {
+                let rankEmoji = findUser.stt === 1 ? "🥇" : findUser.stt === 2 ? "🥈" : findUser.stt === 3 ? "🥉" : "🎀";
+                let msg = `${toBoldUnicode(findUser.name)} >${rankEmoji}\n${toBoldUnicode("𝐫𝐚𝐧𝐤")} ${toBoldNumbers(findUser.stt)} ${toBoldUnicode("𝐰𝐢𝐭𝐡")} ${toBoldNumbers(findUser.count)} ${toBoldUnicode("𝐦𝐞𝐬𝐬𝐚𝐠𝐞𝐬")}`;
+                return message.reply(msg);
+            }
+        }
+    },
 
+    onChat: async ({ usersData, threadsData, event }) => {
+        const { senderID, threadID } = event;
+        const members = await threadsData.get(threadID, "members");
+        const findMember = members.find(user => user.userID == senderID);
+
+        if (!findMember) {
+            members.push({
+                userID: senderID,
+                name: await usersData.getName(senderID),
+                nickname: null,
+                inGroup: true,
+                count: 1
+            });
+        } else {
+            findMember.count += 1;
+        }
+
+        await threadsData.set(threadID, members, "members");
+    }
 };
+
+// Convert numbers to bold Unicode
+function toBoldNumbers(number) {
+    const boldNumbers = {
+        "0": "𝟎", "1": "𝟏", "2": "𝟐", "3": "𝟑", "4": "𝟒", 
+        "5": "𝟓", "6": "𝟖", "7": "𝟕", "8": "𝟖", "9": "𝟗"
+    };
+
+    return number.toString().split("").map(char => boldNumbers[char] || char).join("");
+}
+
+// Convert text to bold Unicode
+function toBoldUnicode(text) {
+    const boldAlphabet = {
+        "a": "𝐚", "b": "𝐛", "c": "𝐜", "d": "𝐝", "e": "𝐞", "f": "𝐟", "g": "𝐠", "h": "𝐡", "i": "𝐢", "j": "𝐣",
+        "k": "𝐤", "l": "𝐥", "m": "𝐦", "n": "𝐧", "o": "𝐨", "p": "𝐩", "q": "𝐪", "r": "𝐫", "s": "𝐬", "t": "𝐭",
+        "u": "𝐮", "v": "𝐯", "w": "𝐰", "x": "𝐱", "y": "𝐲", "z": "𝐳", "A": "𝐀", "B": "𝐁", "C": "𝐂", "D": "𝐃",
+        "E": "𝐄", "F": "𝐅", "G": "𝐆", "H": "𝐇", "I": "𝐈", "J": "𝐉", "K": "𝐊", "L": "𝐋", "M": "𝐌", "N": "𝐍",
+        "O": "𝐎", "P": "𝐏", "Q": "𝐐", "R": "𝐑", "S": "𝐒", "T": "𝐓", "U": "𝐔", "V": "𝐕", "W": "𝐖", "X": "𝐗",
+        "Y": "𝐘", "Z": "𝐙", " ": " ", "'": "'", ",": ",", ".": ".", "-": "-", "!": "!", "?": "?"
+    };
+
+    return text.split('').map(char => boldAlphabet[char] || char).join('');
+}
