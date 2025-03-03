02fad34ab57c1bcae732d762252750434cb9c5a9
mahmudx7 <mahmudx077@gmail.com>
2025-03-03 16:19:34 +0600

    Update and rename bbycc. to bbycc.js

--- scripts/cmds/bbycc.js
+++ scripts/cmds/bbycc.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const baseApiUrl = async () => {
-  const base = await axios.get('https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json');
-  return base.data.api;
+  const base = 'https://www.noobs-api.rf.gd/dipto';
+  return base;
 };
 const prefixes = [
   "bby",
